8e68aa34881302e238d70e81af4af9d1c1ebe37e
Basit Ali <opoali911@gmail.com>
2025-10-25 10:53:03 +0500

    again delete all thing

--- Semester-6/DIP/attendanceProject/backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import all models for easy access
-from app.models.user import User
-from app.models.embedding import Embedding
-from app.models.attendance import Attendance, AttendanceStatus
-
-__all__ = ['User', 'Embedding', 'Attendance', 'AttendanceStatus']
